25d6e3537246d9acd29dc01866779ab70d15950e
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 09:30:07 +0200

    Přidání počítání buildu překladu

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
@@ -70,7 +70,7 @@ import javax.annotation.Nullable;
 public class WorldGuardCommands {
 
     private final WorldGuard worldGuard;
-    private final String build = "0";
+    private final String build = "72";
     @Nullable
     private Sampler activeSampler;
 
