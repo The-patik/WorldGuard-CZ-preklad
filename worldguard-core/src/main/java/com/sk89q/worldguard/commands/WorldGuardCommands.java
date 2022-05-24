bf997f7501bbffa00be23ed8da4547447616027e
root <root@ns97579.hicoria.cloud>
2022-05-24 07:39:09 +0000

    Nový build z jenkins

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
@@ -78,7 +78,7 @@ import static com.sk89q.worldguard.WorldGuard.getPlatform;
 public class WorldGuardCommands {
 
     private final WorldGuard worldGuard;
-    public static int build = 42;
+    public static int build = 43;
     @Nullable
     private Sampler activeSampler;
 
