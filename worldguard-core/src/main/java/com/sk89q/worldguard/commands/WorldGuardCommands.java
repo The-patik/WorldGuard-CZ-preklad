571573621919b44d08093ce4bf16717cdfc72b1d
root <root@ns97579.hicoria.cloud>
2022-06-09 19:22:54 +0000

    Nový build z jenkins

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
@@ -69,7 +69,7 @@ import java.util.regex.Pattern;
 public class WorldGuardCommands {
 
     private final WorldGuard worldGuard;
-    private static int build = 61;
+    private static int build = 62;
     public static int buildnumber = build + 1;
     @Nullable
     private Sampler activeSampler;
