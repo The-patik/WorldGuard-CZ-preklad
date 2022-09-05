e685f7069bbd681a86a09dba3f3830351c5d8727
root <root@ns97579.hicoria.cloud>
2022-09-05 19:05:38 +0000

    Nový build z jenkins

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/WorldGuardCommands.java
@@ -69,7 +69,7 @@ import java.util.regex.Pattern;
 public class WorldGuardCommands {
 
     private final WorldGuard worldGuard;
-    private static int build = 11;
+    private static int build = 12;
     public static int buildnumber = build + 1;
     @Nullable
     private Sampler activeSampler;
